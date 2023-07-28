75e358a3b645bf026e79f0c08b8d6d0845f2fdc3
Veselin Gurguriev <veselin.gurguriev@sirma.bg>
2023-07-28 15:19:17 +0300

    Project set up.

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,49 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+import '../models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
